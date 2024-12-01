3f8886a994c538eb8fd187934de8f8b2c27d66cd
ibrahgamal050 <122299765+ibrahgamal050@users.noreply.github.com>
2024-12-01 11:47:30 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*", // كل المسارات
+        has: [
+          {
+            type: "host",
+            value: "(?<subdomain>.*)\\.meelza\\.com", // الدومين القديم مع دعم الدومينات الفرعية
+          },
+        ],
+        destination: "https://:subdomain.meelza.site/:path*", // تحويل إلى الدومين الجديد بنفس الدومين الفرعي
+        permanent: true, // تحويل دائم 301
+      },
+      {
+        source: "/:path*", // مسار لدعم الطلبات بدون دومينات فرعية
+        has: [
+          {
+            type: "host",
+            value: "meelza.com", // الدومين الأساسي
+          },
+        ],
+        destination: "https://meelza.site/:path*", // تحويل إلى الدومين الجديد بدون فرعي
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
