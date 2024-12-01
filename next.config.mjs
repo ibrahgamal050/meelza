b4f414ca84b090337f79ad5d8a9f6927716f37d7
ibrahgamal050 <122299765+ibrahgamal050@users.noreply.github.com>
2024-12-01 13:26:32 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,16 @@ const nextConfig = {
   async redirects() {
     return [
       {
+        source: 'http://meelza.com/:path*', // Ensure this matches incoming traffic
+        destination: 'https://meelza.site/:path*', // Redirect to new domain
+        permanent: true, // 301 Redirect
+      },
+      {
         source: '/:path*',
         has: [
           {
             type: 'host',
-            value: '(?<subdomain>.*)\\.meelza\\.com',
+            value: '(?<subdomain>.*)\\.meelza\\.com', // Match subdomains
           },
         ],
         destination: 'https://:subdomain.meelza.site/:path*',
